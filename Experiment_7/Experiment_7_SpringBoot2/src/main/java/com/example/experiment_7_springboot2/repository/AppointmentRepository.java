01ccb61d7b9f852bfa8eb80a598d83d9d70289c0
Vipul Raj <163497509+vipulraj04@users.noreply.github.com>
2026-03-16 18:00:14 +0530

    Delete Experiment_7/Experiment_7_SpringBoot2 directory

--- Experiment_7/Experiment_7_SpringBoot2/src/main/java/com/example/experiment_7_springboot2/repository/AppointmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.experiment_7_springboot2.repository;
-
-import com.example.experiment_7_springboot2.entity.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-}
